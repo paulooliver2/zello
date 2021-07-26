ad3c7ab7e0342fa6bda68eb91885c61201080dde
paulo <pauloandre12372@gmail.com>
2021-07-26 12:58:01 -0300

    [ADD] criando validação e criando fuction update

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -4,21 +4,70 @@ namespace App\Http\Controllers;
 
 use \Illuminate\Http\Request;
 use \App\Models\Person;
+use Illuminate\Support\Facades\Log;
 
 class PersonController extends Controller
 {
+
     public function create(Request $request)
     {
-        $param = $request->all();
+        try {
+            $request->validate([
+                'name' => 'required|max:255',
+                'birthdate' => 'required',
+                'cpf' => 'required',
+                'rg' => 'birthdaterequired',
+                'profile' => 'required',
+            ]);
 
-        $person = new Person();
-        $person->name = $param['name'];
-        $person->birthdate = $param['birthdate'];
-        $person->cpf = $param['cpf'];
-        $person->rg = $param['rg'];
-        $person->profile = $param['profile'];
-        $person->save();
+            $param = $request->all();
 
-        return response()->json($person->toArray());
+            $person = new Person();
+            $person->name = $param['name'];
+            $person->birthdate = $param['birthdate'];
+            $person->cpf = $param['cpf'];
+            $person->rg = $param['rg'];
+            $person->profile = $param['profile'];
+            $person->save();
+
+            return response()->json($person->toArray());
+        } catch (\Throwable $e) {
+            Log::channel('stderr')->error($e->getTraceAsString());
+            return response()->json("erro ao criar pessoa", 500);
+        }
     }
+
+    public function all()
+    {
+        return Person::all();
+    }
+
+    public function update(int $id, Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required|max:255',
+                'birthdate' => 'required',
+                'cpf' => 'required',
+                'rg' => 'birthdaterequired',
+                'profile' => 'required',
+            ]);
+
+            $param = $request->all();
+
+            $person = Person::where('id', $id);
+            $person->name = $param['name'];
+            $person->birthdate = $param['birthdate'];
+            $person->cpf = $param['cpf'];
+            $person->rg = $param['rg'];
+            $person->profile = $param['profile'];
+            $person->save();
+
+            return response()->json($person->toArray());
+        } catch (\Throwable $e) {
+            Log::channel('stderr')->error($e->getTraceAsString());
+            return response()->json("erro ao alterar pessoa", 500);
+        }
+    }
+
 }
